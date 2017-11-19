baef0d3c592a6c451672d9484dde62e47d201744
Parker <parkersmathers@yahoo.com>
2017-11-19 17:59:09 -0500

    Expand schema data and add to layout

---
+++ public/_schema.js
@@ -0,0 +1,55 @@
+{
+  "@context": "http://schema.org",
+  "@graph": [
+    {
+      "@type": "Person",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Omaha",
+        "addressRegion": "NE"
+      },
+      "birthPlace": "San Antonio, TX",
+      "email": "mailto:treyemoody@yahoo.com",
+      "image": "https://treymoody.org/images/trey-moody-photo.jpg",
+      "name": "Trey Moody",
+      "sponsor": [
+        {
+          "@type": "Organization",
+          "name": "Academy of American Poets"
+        },
+        {
+          "@type": "Organization",
+          "name": "Slope Editions"
+        },
+        {
+          "@type": "Organization",
+          "name": "University of Nebraska, Lincoln"
+        }
+      ],
+      "url": "https://treymoody.org",
+      "sameAs": [
+        "http://twitter.com/treyismoody",
+        "http://instagram.com/ydoomyert"
+      ],
+      "worksFor": {
+        "@type": "EducationalOrganization",
+        "name": "Creighton University"
+      }
+    },
+    {
+      "@type": "Book",
+      "award": "Kathryn A. Morton Prize in Poetry",
+      "author": "Trey Moody",
+      "copyrightYear": "2014",
+      "image": "thought-that-nature-cover.jpg",
+      "name": "Thought That Nature",
+      "publisher": "Sarabande Books"
+    },
+    {
+      "@type": "Periodical",
+      "name": "Announcement",
+      "publisher": "Gulf Coast",
+      "url": "http://gulfcoastmag.org/journal/28.2/announcement/"
+    }
+  ]
+}
